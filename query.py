d08e6da8fc992fe391b6a6e1fd557e71109a474f
apitts175 <80348388+apitts175@users.noreply.github.com>
2022-03-27 18:54:17 -0500

    added querry, rankBy, and createUrl functions

--- query.py
+++ query.py
@@ -1,21 +1,44 @@
 import requests
+import operator
 
-def rankByCalories():
-	pass
+def createUrl(queryterm):
+    queryterm = queryterm.replace(" ", "%20")
+    url = "https://nutritionix-api.p.rapidapi.com/v1_1/search/" + queryterm
+    return url
+
+def querry(querry):
+    url = createUrl(querry)
+
+    querystring = {"fields":"item_name,item_id,brand_name,nf_calories,nf_serving_weight_grams"}
+
+    headers = {
+        "X-RapidAPI-Host": "nutritionix-api.p.rapidapi.com",
+        "X-RapidAPI-Key": "2f50ca2397mshff1a209d8be093ap1c0772jsn0f2d7d8caca4"
+    }
+
+    response = requests.request("GET", url, headers=headers, params=querystring)
+    dictionary = response.json()
+    for i in dictionary["hits"]:
+        print(i)
+    return dictionary["hits"]
+
+def rankBy(hits, sortingTerm):
+    caloryRank = {}
+    for i in hits:
+        caloryRank[i['_id']] = i["fields"][sortingTerm]/i["fields"]["nf_serving_size_qty"]/i["fields"]["nf_serving_weight_grams"]
+
+    sortedDict = dict(sorted(caloryRank.items(),key=operator.itemgetter(1),reverse=True))
+    for i in sortedDict:
+        for item in hits:
+            if item['_id'] == i:
+                print(item['fields']['item_name'],sortedDict[i])
+    return sortedDict 
 
 
 
+# get user input, dummy input rn since front end not created yet
+queryterm = "cheddar cheese"
 
-url = "https://nutritionix-api.p.rapidapi.com/v1_1/search/cheddar%20cheese"
+hits = querry(queryterm)
 
-querystring = {"fields":"item_name,item_id,brand_name,nf_calories,nf_total_fat"}
-
-headers = {
-	"X-RapidAPI-Host": "nutritionix-api.p.rapidapi.com",
-	"X-RapidAPI-Key": "274e0fe4e2msh35a56d59a1bc9b5p1b76c1jsn87091e6b16fa"
-}
-
-response = requests.request("GET", url, headers=headers, params=querystring)
-
-print(response.json())
-print(type(response.json()))
+print(rankBy(hits, "nf_calories"))
