2457ec9b50f8fb0c4f68c1ab2f0af0dccfb51912
Ronald Lee <55027200+leeroddy17@users.noreply.github.com>
2022-04-04 02:15:06 -0500

    Play with Flask UI

---
+++ homepage.py
@@ -0,0 +1,16 @@
+from flask import Flask, redirect, url_for, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/test")
+def test():
+    return render_template("newpage.html")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
